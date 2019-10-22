ed171bf26471d96d34fe1afdb5ce5bd4fd9b66d6
Derek Reidy <50381777+DerekJR321@users.noreply.github.com>
2019-10-22 02:05:22 -0400

    Add files via upload

---
+++ phpcart.sql
@@ -0,0 +1,282 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 22, 2019 at 08:03 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `phpcart`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `last_name` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `phone` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+  `address` text COLLATE utf8_unicode_ci NOT NULL,
+  `city` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `state` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `zip` int(15) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  `status` enum('1','0') COLLATE utf8_unicode_ci NOT NULL DEFAULT '1' COMMENT '1=Active | 0=Inactive'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`id`, `first_name`, `last_name`, `email`, `phone`, `address`, `city`, `state`, `zip`, `created`, `modified`, `status`) VALUES
+(1, 'Derek', 'Reidy', 'moop@poop.com', '555-555-5555', '123 Test Street', '', '', 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `grand_total` float(10,2) NOT NULL,
+  `created` datetime NOT NULL,
+  `status` enum('Pending','Completed','Cancelled') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `customer_id`, `grand_total`, `created`, `status`) VALUES
+(1, 1, 20.50, '2019-09-30 02:43:03', 'Pending');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `quantity` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `order_items`
+--
+
+INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `quantity`) VALUES
+(1, 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `product_type` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `price` float(10,2) NOT NULL,
+  `image` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  `status` enum('1','0') COLLATE utf8_unicode_ci NOT NULL DEFAULT '1' COMMENT '1=Active | 0=Inactive'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `description`, `product_type`, `price`, `image`, `created`, `modified`, `status`) VALUES
+(1, 'Adult Male Bike', '', 'bike', 20.50, 'http://via.placeholder.com/250x250?text=Adult%20Male%20Bike', '2019-09-30 00:00:00', '0000-00-00 00:00:00', '1'),
+(2, 'Adult Female Bike', '', 'bike', 20.50, 'http://via.placeholder.com/250x250?text=Adult%20Female%20Bike', '2019-09-30 00:00:00', '2019-09-30 01:00:00', '1'),
+(3, 'Kids Unisex Bike', '', 'bike', 12.75, 'http://via.placeholder.com/250x250?text=Kids%20Unisex%20Bike', '2019-09-29 00:00:00', '2019-09-30 00:00:00', '1'),
+(4, 'Adult Unisex Helmet', '', 'accessory', 4.00, 'http://via.placeholder.com/250x250?text=Adult%20Unisex%20Helmet', '2019-09-29 00:00:00', '2019-09-30 00:00:00', '1'),
+(5, 'Kids Unisex Helmet', '', 'accessory', 3.50, 'http://via.placeholder.com/250x250?text=Kids%20Unisex%20Helmet', '2019-09-29 00:00:00', '2019-09-30 00:00:00', '1'),
+(6, 'Insurance', '', 'addon', 9.99, 'http://via.placeholder.com/250x250?text=Insurance', '2019-09-29 00:00:00', '2019-09-30 00:00:00', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `states`
+--
+
+CREATE TABLE `states` (
+  `id` int(11) NOT NULL,
+  `abbr` varchar(2) NOT NULL,
+  `name` varchar(250) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `states`
+--
+
+INSERT INTO `states` (`id`, `abbr`, `name`) VALUES
+(1, 'AL', 'Alabama'),
+(2, 'AK', 'Alaska'),
+(3, 'AZ', 'Arizona'),
+(4, 'AR', 'Arkansas'),
+(5, 'CA', 'California'),
+(6, 'CO', 'Colorado'),
+(7, 'CT', 'Connecticut'),
+(8, 'DE', 'Delaware'),
+(9, 'DC', 'District of Columbia'),
+(10, 'FL', 'Florida'),
+(11, 'GA', 'Georgia'),
+(12, 'HI', 'Hawaii'),
+(13, 'ID', 'Idaho'),
+(14, 'IL', 'Illinois'),
+(15, 'IN', 'Indiana'),
+(16, 'IA', 'Iowa'),
+(17, 'KS', 'Kansas'),
+(18, 'KY', 'Kentucky'),
+(19, 'LA', 'Louisiana'),
+(20, 'ME', 'Maine'),
+(21, 'MD', 'Maryland'),
+(22, 'MA', 'Massachusetts'),
+(23, 'MI', 'Michigan'),
+(24, 'MN', 'Minnesota'),
+(25, 'MS', 'Mississippi'),
+(26, 'MO', 'Missouri'),
+(27, 'MT', 'Montana'),
+(28, 'NE', 'Nebraska'),
+(29, 'NV', 'Nevada'),
+(30, 'NH', 'New Hampshire'),
+(31, 'NJ', 'New Jersey'),
+(32, 'NM', 'New Mexico'),
+(33, 'NY', 'New York'),
+(34, 'NC', 'North Carolina'),
+(35, 'ND', 'North Dakota'),
+(36, 'OH', 'Ohio'),
+(37, 'OK', 'Oklahoma'),
+(38, 'OR', 'Oregon'),
+(39, 'PA', 'Pennsylvania'),
+(40, 'RI', 'Rhode Island'),
+(41, 'SC', 'South Carolina'),
+(42, 'SD', 'South Dakota'),
+(43, 'TN', 'Tennessee'),
+(44, 'TX', 'Texas'),
+(45, 'UT', 'Utah'),
+(46, 'VT', 'Vermont'),
+(47, 'VA', 'Virginia'),
+(48, 'WA', 'Washington'),
+(49, 'WV', 'West Virginia'),
+(50, 'WI', 'Wisconsin'),
+(51, 'WY', 'Wyoming');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `customer_id` (`customer_id`);
+
+--
+-- Indexes for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `order_id` (`order_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `states`
+--
+ALTER TABLE `states`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `order_items`
+--
+ALTER TABLE `order_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `states`
+--
+ALTER TABLE `states`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
